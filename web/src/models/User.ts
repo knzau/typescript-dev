2abfc2fde1ff7aa1ab3a5a50f0d317da722e0c8b
Ken Nzau <kenn.nzau@gmail.com>
2024-03-24 19:39:23 -0300

    refactor User model to includ collection builder

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -4,6 +4,7 @@ import { Attributes } from "./Attributes";
 import { Events } from "./Events";
 import { ApiSync } from "./ApiSync";
 import { Model } from "./Model";
+import { Collection } from "./Collection";
 
 export interface UserProps {
 	name?: string;
@@ -16,5 +17,9 @@ const rootURL = "http://localhost:3001/users";
 export class User extends Model<UserProps> {
 	static buildUser(attrs: UserProps): User {
 		return new User(new Attributes<UserProps>(attrs), new Events(), new ApiSync<UserProps>(rootURL));
+	}
+
+	static buildUserCollection(): Collection<User, UserProps> {
+		return new Collection<User, UserProps>(rootURL, (json: UserProps) => User.buildUser(json));
 	}
 }
